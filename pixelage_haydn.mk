f7a1a07678d1d5f20b9d69746917839cc111087e
Thunderstorm1708 <89205550+Thunderstorm1708@users.noreply.github.com>
2024-11-27 16:01:28 +0530

    haydn: Bringup Project Pixelage

--- pixelage_haydn.mk
+++ pixelage_haydn.mk
@@ -11,14 +11,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from haydn device
 $(call inherit-product, device/xiaomi/haydn/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Pixelage stuff.
+$(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
+
+# Pixelage
+PIXELAGE_BUILDTYPE := UNOFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := haydn
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2012K11G
-PRODUCT_NAME := lineage_haydn
+PRODUCT_NAME := pixelage_haydn
 
 PRODUCT_SYSTEM_NAME := haydn_global
 PRODUCT_SYSTEM_DEVICE := haydn
@@ -35,5 +40,3 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Sign Builds
-PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/xiaomi/priv-keys/releasekey
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 echo 'Starting to clone stuffs needed to build for Haydn'
 
 # Common Tree
-git clone --depth=1 https://github.com/xiaomi-haydn-devs/android_device_xiaomi_sm8350-common -b lineage-22 device/xiaomi/sm8350-common
+git clone --depth=1 https://github.com/Thunderstorm1708/android_device_xiaomi_sm8350-common_15 -b Pixelage-15 device/xiaomi/sm8350-common
 
 # Haydn Vendor
 git clone --depth=1 https://github.com/xiaomi-haydn-devs/proprietary_vendor_xiaomi_haydn -b lineage-22 vendor/xiaomi/haydn
@@ -19,15 +19,22 @@ git clone --depth=1 https://gitlab.com/Alucard_Storm/vendor_xiaomi_haydn-firmwar
 
 # Xiaomi
 echo 'Cloning hardware xiaomi'
-rm -rf hardware/xiaomi && git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
+rm -rf hardware/xiaomi && git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.0 hardware/xiaomi
 
 # Camera
 echo 'Cloning Leica camera'
 git clone --depth=1 https://gitlab.com/Alucard_Storm/haydn-miuicamera -b fourteen-leica vendor/xiaomi/haydn-miuicamera
 rm -rf hardware/xiaomi/megvii
 
-# Signature Keys
-echo 'Cloning Signing keys'
-git clone --depth=1 https://github.com/xiaomi-haydn-devs/priv_keys.git vendor/xiaomi/priv-keys
+# Priv-keys
+echo 'Cloning private keys'
+git clone https://github.com/1xtAsh/vendor_lineage-priv_keys -b lineage-21 vendor/pixelage-priv/keys
+
+# Leica patch
+echo 'Adding Leica camera patch'
+cd frameworks/base
+wget https://raw.githubusercontent.com/xiaomi-haydn-devs/Patch-Haydn/14/Leicamera/0002-Expose-aux-camera-if-packagename-is-null.patch
+patch -p1 <0002-Expose-aux-camera-if-packagename-is-null.patch
+cd ../..
 
 echo 'delete vendorsetup.sh from device tree once this is done'
